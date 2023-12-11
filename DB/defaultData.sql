1090373923db47362ded455f7355126acf725581
Your Name <you@example.com>
2023-12-11 11:55:35 -0700

    a

--- DB/defaultData.sql
+++ DB/defaultData.sql
@@ -8,10 +8,12 @@ INSERT INTO seller (user_id, status) values (2, 1);
 INSERT INTO config (app_seller_id, status) values (1, 1);
 
 
-INSERT INTO product (`key`, price, status) values ("h1", 110, 1);
-INSERT INTO product (`key`, price, status) values ("h2", 120, 1);
-INSERT INTO product (`key`, price, status) values ("h3", 130, 1);
+INSERT INTO product (`key`, price, status) values ("h1", 60, 1);
+INSERT INTO product (`key`, price, status) values ("h2", 65 , 1);
+INSERT INTO product (`key`, price, status) values ("h3", 50, 1);
+INSERT INTO product (`key`, price, status) values ("h4", 60, 1);
 
-INSERT INTO product (`key`, price, status) values ("hm1", 210, 1);
-INSERT INTO product (`key`, price, status) values ("hm2", 220, 1);
-INSERT INTO product (`key`, price, status) values ("hm3", 230, 1);
+INSERT INTO product (`key`, price, status) values ("hm1", 100, 1);
+INSERT INTO product (`key`, price, status) values ("hm2", 90, 1);
+INSERT INTO product (`key`, price, status) values ("hm3", 95, 1);
+INSERT INTO product (`key`, price, status) values ("hm4", 80, 1);
